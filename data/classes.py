a1f22252792d594f7b51a31e64e9df48a4e8df78
Dasha <baklanovadariya@mail.ru>
2025-03-01 11:07:28 +0300

    ...

--- data/classes.py
+++ data/classes.py
@@ -1,5 +1,5 @@
+from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
-from forms.Ui import Ui_MainWindow
 from PyQt6.QtWidgets import QMainWindow
 from random import randint
 
@@ -7,13 +7,13 @@ from random import randint
 SIZE = [800, 600]
 
 
-class MyWidget(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('forms/Ui.ui', self)
         self.n = randint(10, 20)
         self.flag = False
-        self.setWindowTitle('Git и случайные окружности')
+        self.setWindowTitle('Git и желтые окружности')
         self.btn.clicked.connect(self.draw)
 
 
@@ -24,11 +24,11 @@ class MyWidget(QMainWindow, Ui_MainWindow):
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
+        self.color = (255, 255, 0)
+        qp.setPen(QColor(*self.color))
+        qp.setBrush(QColor(*self.color))
         if self.flag:
             for i in range(self.n):
-                self.color = (randint(0, 256), randint(0, 256), randint(0, 256))
-                qp.setPen(QColor(*self.color))
-                qp.setBrush(QColor(*self.color))
                 self.x, self.y = randint(50, SIZE[0] - 50), randint(50, SIZE[1] - 50)
                 self.size = randint(10, 50)
                 qp.drawEllipse(self.x, self.y, self.size, self.size)
